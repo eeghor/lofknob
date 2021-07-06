a5510b8d99710744b1b62dae4811d129894b804d
Igor Korostil <eeghor@gmail.com>
2021-07-06 23:23:59 +1000

    pilot two

--- lofknob.py
+++ lofknob.py
@@ -5,7 +5,7 @@ from sklearn.neighbors import LocalOutlierFactor
 def lofknob(X, c_grid=None, k_grid=None):
 
     c_grid = np.linspace(0.01, 0.15, num=10)
-    k_grid = np.linspace(10, 50, num=20)
+    k_grid = np.arange(10, 50, step=2)
     
     # number of rows
     n = X.shape[0]
@@ -17,23 +17,24 @@ def lofknob(X, c_grid=None, k_grid=None):
         # with contamination c, floor(c*n) rows will be labelled as outliers; e.g. if n=98 and c=0.12 then 
         # floor(c*n=0.12*98=11.76)=11 rows will be ouliers
     
-        cn = np.floor(c*n)
+        cn = np.floor(c*n).astype(int)
     
         m_c_out = v_c_out = 0
         m_c_in = v_c_in = 0
         
-        t_c = np.zero_like(k_grid)
+        t_c = np.empty_like(k_grid)
     
-        k_grid_len = k_grid.shape[1]
+        k_grid_len = k_grid.shape[0]
         
         for k in k_grid:
             
             # fit LOF with c and k and return labels, -1 if outlier and 1 otherwise
-            lof = LocalOutlierFactor(contamination=c, n_neighbors=k).fit(X)
-            labels = lof.predict(X)
+            lof = LocalOutlierFactor(contamination=c, n_neighbors=k)
+            labels = lof.fit_predict(X)
             
             # ln(LOFs for each row)
             ln_lofs = np.log(-lof.negative_outlier_factor_)
+
             # LOFs for outliers
             out_ln_lofs = ln_lofs[labels==-1] 
             # top cn LOFs for inliers
@@ -51,12 +52,12 @@ def lofknob(X, c_grid=None, k_grid=None):
             v_c_k_in = np.var(in_ln_lofs)
             v_c_in += v_c_k_in/k_grid_len
             
-            t_c_k = (m_c_k_out - m_c_k_in)/np.sqrt((v_c_k_out + v_c_k_in)/cn)
+            t_c_k = (m_c_k_out - m_c_k_in)/np.sqrt((v_c_k_out + v_c_k_in)/cn) if (m_c_k_out - m_c_k_in)*(v_c_k_out + v_c_k_in) > 1e-8 else 0
             
-            t_c = t_c.append(t_c_k)
+            t_c = np.append(t_c, t_c_k)
             
         # non-centrality parameter
-        ncp_c = (m_c_out - m_c_in)/np.sqrt((v_c_out + v_c_in)/cn)
+        ncp_c = (v_c_out + v_c_in)/np.sqrt((v_c_out + v_c_in)/cn) if (v_c_out + v_c_in)*(v_c_out + v_c_in) > 1e-8 else 0
     
         # degrees of freedom
         df_c = 2*cn - 2
@@ -73,6 +74,4 @@ def lofknob(X, c_grid=None, k_grid=None):
     c_opt, k_opt, _ = max(candidates, key=lambda x: x[2])
 
     return (c_opt, k_opt)
-    
-
         
