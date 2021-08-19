32644a9deed3d28d8e95de39928281a75ca17028
Igor Korostil <eeghor@gmail.com>
2021-08-19 14:33:33 +1000

    walrus bug fixed

--- lofknob.py
+++ lofknob.py
@@ -17,19 +17,19 @@ class lofknob:
     usage example
     -------------
 
-    optimal_contamination, optimal_number_of_neighbours = lofknob().tune(X, c_grid, k_grid)
+    optimal_contamination, optimal_number_of_neighbours = lofknob().tune(X, c_grid, k_grid, return_scores)
 
     where
 
     X:
-        is a pandas DataFrame with training data
+            is a pandas DataFrame with training data
     c_grid:
-        is a list of contamination values to try, e.g. [0.01, 0.02, ..]
+            is a list of contamination values to try, e.g. [0.01, 0.02, ..]
     k_grid:
-        is a list of the number of neighbours to try, e.g. [5, 10, 20, ..]
+            is a list of the number of neighbours to try, e.g. [5, 10, 20, ..]
     return_scores:
-        if True, return a list of parameter pairs competing for being "optimal" along
-        with their probability scores; default: False
+            if True, return a list of parameter pairs competing for being "optimal" along
+            with their probability scores; default: False
     """
 
     # scikit-learn restricts contamination to range (0, 0.5]
@@ -90,15 +90,17 @@ class lofknob:
 
         for c in c_grid:
 
-            # with contamination c, cn=floor(c*n_rows) rows to be labelled outliers
+            # with contamination c, expected_outlier_rows=floor(c*n_rows)
             # e.g. if n_rows=98 and c=0.12 then
-            # cn=floor(c*n_rows=0.12*98=11.76)=11
+            # expected_outlier_rows=floor(c*n_rows=0.12*98=11.76)=11
 
-            cn = np.floor(c * n_rows).astype(int)  # np.floor() returns a float
+            expected_outlier_rows = np.floor(c * n_rows).astype(
+                int
+            )  # np.floor() returns a float
 
-            if cn < lofknob.MIN_OUTLIER_ROWS:
+            if expected_outlier_rows < lofknob.MIN_OUTLIER_ROWS:
                 print(
-                    f"contamination {c:.5f} results in {cn:,} outliers (required minimum is {lofknob.MIN_OUTLIER_ROWS})"
+                    f"contamination {c:.5f} results in {expected_outlier_rows:,} outliers (required minimum is {lofknob.MIN_OUTLIER_ROWS})"
                 )
                 continue
 
@@ -128,9 +130,11 @@ class lofknob:
                     continue
 
                 # ..and for inliers but these will be sorted smallest to largest;
-                # then we pick the last cn of them since after the sorting
-                # these will be the largest cn LOF scores
-                inl_lls = heapq.nlargest(cn, lls[labels == lofknob.INLIER_LABEL])
+                # then we pick the last expected_outlier_rows of them since after the sorting
+                # these will be the largest expected_outlier_rows LOF scores
+                inl_lls = heapq.nlargest(
+                    expected_outlier_rows, lls[labels == lofknob.INLIER_LABEL]
+                )
 
                 # calculate mean and variance of lls for outliers
                 out_mean_lls_this_k = np.mean(out_lls).tolist()
@@ -144,16 +148,16 @@ class lofknob:
                 in_var_lls_this_k = np.var(inl_lls).tolist()
                 in_var_lls_all_ks.append(in_var_lls_this_k)
 
-                # measure distance between outliers and the nearest cn inliers
+                # measure distance between outliers and the nearest expected_outlier_rows inliers
                 if (
-                    diff_means_this_k := out_mean_lls_this_k - in_mean_lls_this_k
+                    (diff_means_this_k := out_mean_lls_this_k - in_mean_lls_this_k)
                     > lofknob.VERY_SMALL
                 ) and (
-                    sum_vars_this_k := out_var_lls_this_k + in_var_lls_this_k
+                    (sum_vars_this_k := out_var_lls_this_k + in_var_lls_this_k)
                     > lofknob.VERY_SMALL
                 ):
                     dist_score_this_k = diff_means_this_k / np.sqrt(
-                        sum_vars_this_k / cn
+                        sum_vars_this_k / expected_outlier_rows
                     )
                 else:
                     dist_score_this_k = 0
@@ -161,15 +165,21 @@ class lofknob:
                 dist_scores_all_k.append((k, dist_score_this_k))
 
             if (
-                diff_mean_means_over_ks := np.mean(out_mean_lls_all_ks)
-                - np.mean(in_mean_lls_all_ks)
+                (
+                    diff_mean_means_over_ks := np.mean(out_mean_lls_all_ks)
+                    - np.mean(in_mean_lls_all_ks)
+                )
                 > lofknob.VERY_SMALL
             ) and (
-                sum_mean_vars_over_ks := np.mean(out_var_lls_all_ks)
-                + np.mean(in_var_lls_all_ks)
+                (
+                    sum_mean_vars_over_ks := np.mean(out_var_lls_all_ks)
+                    + np.mean(in_var_lls_all_ks)
+                )
                 > lofknob.VERY_SMALL
             ):
-                ncp_c = diff_mean_means_over_ks / np.sqrt(sum_mean_vars_over_ks / cn)
+                ncp_c = diff_mean_means_over_ks / np.sqrt(
+                    sum_mean_vars_over_ks / expected_outlier_rows
+                )
             else:
                 ncp_c = 0
 
@@ -178,7 +188,7 @@ class lofknob:
             k_opt_this_c, opt_dist_score = max(dist_scores_all_k, key=lambda x: x[1])
 
             # degrees of freedom
-            df_this_c = 2 * cn - 2
+            df_this_c = 2 * expected_outlier_rows - 2
 
             p_c = nct.cdf(x=opt_dist_score, df=df_this_c, nc=ncp_c, loc=0, scale=1)
 
