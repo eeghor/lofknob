371b54df5396a142a04b89c9acc63bb1e78b881b
Igor Korostil <eeghor@gmail.com>
2021-08-23 23:34:50 +1000

    more examples

--- lof_compare.py
+++ lof_compare.py
@@ -19,3 +19,36 @@ else:
 print(f"total data points: {len(X.index)}")
 print(f"contamination: {_counter[-5]} samples, {_counter[-5]/len(X.index):.4f}")
 print()
+
+# https://github.com/scikit-learn/scikit-learn/blob/114616d9f6ce9eba7c1aacd3d4a254f868010e25/examples/neighbors/plot_lof_outlier_detection.py
+
+np.random.seed(42)
+
+# Generate train data
+X_inliers = 0.3 * np.random.randn(100, 2)
+X_inliers = np.r_[X_inliers + 2, X_inliers - 2]
+
+# Generate some outliers
+X_outliers = np.random.uniform(low=-4, high=4, size=(20, 2))
+X = pd.DataFrame(np.r_[X_inliers, X_outliers])
+
+n_outliers = len(X_outliers)
+ground_truth = np.ones(len(X), dtype=int)
+ground_truth[-n_outliers:] = -1
+
+if res := lofknob().tune(X, c_grid, k_grid):
+    c_opt, k_opt = res
+    print(f'optimal contamination: {c_opt:.4f}, number of neighbours: {k_opt:.0f}')
+else:
+    print("optimisation failed")
+
+print(f"outliers: {n_outliers}, contamination: {n_outliers/len(X_inliers):.4f}")
+
+# fit the model for outlier detection (default)
+# clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+# use fit_predict to compute the predicted labels of the training samples
+# (when LOF is used for outlier detection, the estimator has no predict,
+# decision_function and score_samples methods).
+# y_pred = clf.fit_predict(X)
+# n_errors = (y_pred != ground_truth).sum()
+# X_scores = clf.negative_outlier_factor_
