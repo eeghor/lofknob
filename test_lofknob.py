b5c59083847fdcfdbf8baa9c287042b8956cbf28
Igor Korostil <eeghor@gmail.com>
2021-08-25 17:12:28 +1000

    more details

--- test_lofknob.py
+++ test_lofknob.py
@@ -3,6 +3,7 @@ from lofknob import lofknob
 import pandas as pd
 from collections import Counter
 import numpy as np
+from scipy.stats import norm, uniform
 
 
 class ObviousOutliers(unittest.TestCase):
@@ -10,58 +11,29 @@ class ObviousOutliers(unittest.TestCase):
 
         self.OUTLIER = -1
         self.INLIER = 1
-        self.OUTLIER_PROPORTION = 0.06
-        self.TOTAL_DATAPOINTS = 400
-        self.EXPECTED_OUTLIERS = int(
-            np.floor(self.OUTLIER_PROPORTION * self.TOTAL_DATAPOINTS)
-        )
 
-        while 1:
-            self.data = pd.DataFrame(
-                {
-                    "a": np.random.choice(
-                        a=[self.OUTLIER, self.INLIER],
-                        size=self.TOTAL_DATAPOINTS,
-                        p=[self.OUTLIER_PROPORTION, 1 - self.OUTLIER_PROPORTION],
-                    )
-                }
-            )
+    def test_one(self):
 
-            self.REAL_CONTAMINATION = Counter(self.data["a"]).get(
-                self.OUTLIER, 0
-            ) / len(self.data)
-            if abs(self.REAL_CONTAMINATION - self.OUTLIER_PROPORTION) < 0.001:
-                break
+        samples = 400
+        outliers = 32
+        sample_contamination = outliers / samples
+        _actual_contamination = 0
 
-        self.c_grid = np.linspace(0.01, 0.20, 20)
-        self.k_grid = [5, 10, 15, 20, 25, 30]
+        while abs(_actual_contamination - sample_contamination) > 0.001:
+            data = np.random.choice(
+                        a=[-1, 1],
+                        size=samples,
+                        p=[sample_contamination, 1 - sample_contamination]).reshape(-1, 1)
 
-    def test_settings_sanity(self):
+            _actual_contamination = Counter(data.flatten()).get(-1, 0) / len(data)
 
-        self.assertAlmostEqual(
-            self.OUTLIER_PROPORTION, self.REAL_CONTAMINATION, places=2
-        )
-        self.assertEqual(
-            self.EXPECTED_OUTLIERS / self.TOTAL_DATAPOINTS, self.REAL_CONTAMINATION
-        )
-        self.assertLessEqual(max(self.c_grid), 0.5)
-        self.assertGreater(min(self.c_grid), 0)
-
-    def test_optimal_contamination(self):
-
-        c_opt, k_opt = lofknob().tune(
-            X=self.data, c_grid=self.c_grid, k_grid=self.k_grid, verbose=True
-        )
-        print(f"optimal contamination: {c_opt}, optimal neighbours: {k_opt}")
-
-        self.assertAlmostEqual(c_opt, self.REAL_CONTAMINATION, places=2)
-
-    def test_optimal_contamination_scores(self):
+        c_grid = [0.01, 0.02, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.20]
+        k_grid = [5, 8, 10, 15, 20, 30]
 
         if candidates_with_scores := lofknob().tune(
-            X=self.data,
-            c_grid=self.c_grid,
-            k_grid=self.k_grid,
+            X=data,
+            c_grid=c_grid,
+            k_grid=k_grid,
             verbose=True,
             return_scores=True,
         ):
@@ -70,10 +42,35 @@ class ObviousOutliers(unittest.TestCase):
                 candidates_with_scores, key=lambda x: x.probability_score
             )
 
-            print(candidates_with_scores)
-            print(f"optimal contamination: {c_opt}, optimal neighbours: {k_opt}")
+            self.assertAlmostEqual(c_opt, _actual_contamination, places=2)
 
-            self.assertAlmostEqual(c_opt, self.REAL_CONTAMINATION, places=2)
+    # def test_two(self):
+
+    #     samples = 500
+    #     outliers = 25
+    #     sample_contamination = outliers / samples
+
+    #     data = pd.concat(
+    #         [
+    #             pd.DataFrame({"a": norm().rvs(size=samples - outliers)}),
+    #             pd.DataFrame({"a": uniform(loc=5).rvs(size=outliers)}),
+    #         ]
+    #     )
+
+    #     c_grid = [0.01, 0.02, 0.05, 0.06, 0.07, 0.10, 0.12, 0.15, 0.20]
+    #     k_grid = [5, 8, 10, 15, 20, 30]
+
+    #     candidates_with_scores = lofknob().tune(
+    #         X=data,
+    #         c_grid=c_grid,
+    #         k_grid=k_grid,
+    #         verbose=True,
+    #         return_scores=True,
+    #     )
+
+    #     c_opt, k_opt, _ = max(candidates_with_scores, key=lambda x: x.probability_score)
+
+    #     self.assertAlmostEqual(c_opt, sample_contamination, places=2)
 
 
 if __name__ == "__main__":
